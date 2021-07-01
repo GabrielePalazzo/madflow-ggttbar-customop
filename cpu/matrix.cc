9393283f298e5e134c9ca0777e40b6524c39c634
Gabriele <gabriele.palazzo6.github@gmail.com>
2021-07-01 14:59:29 +0200

    Added basic op stuff

--- cpu/matrix.cc
+++ cpu/matrix.cc
@@ -1,6 +1,38 @@
 #include "tensorflow/core/framework/op.h"
+#include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/shape_inference.h"
 
-class Matrix_1_gg_ttx {
+using namespace tensorflow;
 
+// x = tf.random.uniform([10], dtype=tf.float64)
+// x = float_me(np.random.rand(10))
+
+REGISTER_OP("Matrix")
+    .Input("to_zero: double")
+    .Output("zeroed: double")
+    .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
+      c->set_output(0, c->input(0));
+      return Status::OK();
+    });
+    
+class MatrixOp : public OpKernel {
+ public:
+  explicit MatrixOp(OpKernelConstruction* context) : OpKernel(context) {}
+
+  void Compute(OpKernelContext* context) override {
+    // Grab the input tensor
+    const Tensor& input_tensor = context->input(0);
+    auto input = input_tensor.flat<double>();
+
+    // Create an output tensor
+    Tensor* output_tensor = NULL;
+    OP_REQUIRES_OK(context, context->allocate_output(0, input_tensor.shape(),
+                                                     &output_tensor));
+    auto output_flat = output_tensor->flat<double>();
+
+    // Do some stuff here...
+
+  }
 };
+
+REGISTER_KERNEL_BUILDER(Name("Matrix").Device(DEVICE_CPU), MatrixOp);
