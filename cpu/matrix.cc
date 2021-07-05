5f0b2703a219736a8b2ab07632aa195dd1726098
Gabriele <gabriele.palazzo6.github@gmail.com>
2021-07-05 18:33:35 +0200

    Added basic vxxxxx function

--- cpu/matrix.cc
+++ cpu/matrix.cc
@@ -22,7 +22,8 @@ REGISTER_OP("Matrix")
       return Status::OK();
     });
 
-Tensor matrix();
+Tensor matrix(const double*, const double*, const double*, const double*, const complex128*, const complex128*);
+Tensor vxxxxx(double, double, double, double);
     
 class MatrixOp : public OpKernel {
  public:
@@ -30,20 +31,35 @@ class MatrixOp : public OpKernel {
 
   void Compute(OpKernelContext* context) override {
     // Grab the input tensor
-    const Tensor& all_ps = context->input(0);
-    auto all_ps_flat = all_ps.flat<double>();
+    const Tensor& all_ps_tensor = context->input(0);
+    auto all_ps = all_ps_tensor.flat<double>().data();
+    
+    const Tensor& hel_tensor = context->input(1);
+    auto hel = hel_tensor.flat<double>().data();
+    
+    const Tensor& mdl_MT_tensor = context->input(2);
+    auto mdl_MT = mdl_MT_tensor.flat<double>().data();
+    
+    const Tensor& mdl_WT_tensor = context->input(3);
+    auto mdl_WT = mdl_WT_tensor.flat<double>().data();
+    
+    const Tensor& GC_10_tensor = context->input(4);
+    auto GC_10 = GC_10_tensor.flat<complex128>().data();
+    
+    const Tensor& GC_11_tensor = context->input(5);
+    auto GC_11 = GC_11_tensor.flat<complex128>().data();
 
     // Create an output tensor
     Tensor* output_tensor = NULL;
-    OP_REQUIRES_OK(context, context->allocate_output(0, all_ps.shape(),
+    OP_REQUIRES_OK(context, context->allocate_output(0, all_ps_tensor.shape(),
                                                      &output_tensor));
     auto output_flat = output_tensor->flat<double>();
 
-    matrix();
+    matrix(all_ps, hel, mdl_MT, mdl_WT, GC_10, GC_11);
   }
 };
 
-Tensor matrix() {
+Tensor matrix(const double* all_ps, const double* hel, const double* mdl_MT, const double* mdl_WT, const complex128* GC_10, const complex128* GC_11) {
     int ngraphs = 3;
     int nwavefuncs = 5;
     int ncolor = 2;
@@ -59,6 +75,14 @@ Tensor matrix() {
     cf.vec<complex128>()(2) = -2;
     cf.vec<complex128>()(3) = 16;
     
+    // Begin code
+    
+    //Tensor w0 = vxxxxx(all_ps[:,0],ZERO,hel[0],-1)
+    
+    return Tensor(); // dummy tensor
+}
+
+Tensor vxxxxx(double p, double vmass, double nhel, double nsv) {
     return Tensor(); // dummy tensor
 }
 
