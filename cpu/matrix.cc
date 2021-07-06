5c25edf03ec9ce5c2a5d8fb05123e88fe05829a7
Gabriele <gabriele.palazzo6.github@gmail.com>
2021-07-06 15:10:45 +0200

    Updated matrix.cc

--- cpu/matrix.cc
+++ cpu/matrix.cc
@@ -1,7 +1,8 @@
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/shape_inference.h"
-//#include "tensorflow/cc/ops/math_ops.h"
+#include "tensorflow/cc/ops/array_ops.h"
+#include "tensorflow/cc/ops/math_ops.h"
 
 #include <vector>
 
@@ -22,8 +23,9 @@ REGISTER_OP("Matrix")
       return Status::OK();
     });
 
-Tensor matrix(const double*, const double*, const double*, const double*, const complex128*, const complex128*);
-Tensor vxxxxx(double, double, double, double);
+int nevents = 2;
+Tensor matrix(const double*, const double*, const double, const double, const complex128*, const complex128*);
+Tensor vxxxxx(double*, double, double, double);
     
 class MatrixOp : public OpKernel {
  public:
@@ -55,11 +57,11 @@ class MatrixOp : public OpKernel {
                                                      &output_tensor));
     auto output_flat = output_tensor->flat<double>();
 
-    matrix(all_ps, hel, mdl_MT, mdl_WT, GC_10, GC_11);
+    matrix(all_ps, hel, *mdl_MT, *mdl_WT, GC_10, GC_11);
   }
 };
 
-Tensor matrix(const double* all_ps, const double* hel, const double* mdl_MT, const double* mdl_WT, const complex128* GC_10, const complex128* GC_11) {
+Tensor matrix(const double* all_ps, const double* hel, const double mdl_MT, const double mdl_WT, const complex128* GC_10, const complex128* GC_11) {
     int ngraphs = 3;
     int nwavefuncs = 5;
     int ncolor = 2;
@@ -77,12 +79,44 @@ Tensor matrix(const double* all_ps, const double* hel, const double* mdl_MT, con
     
     // Begin code
     
-    //Tensor w0 = vxxxxx(all_ps[:,0],ZERO,hel[0],-1)
+    for (int i = 0; i < nevents; i++) {
+        double all_ps_0[4];
+        double all_ps_1[4];
+        double all_ps_2[4];
+        double all_ps_3[4];
+        for (int j = 0; j < 4; j++) {
+            all_ps_0[j] = all_ps[16 * i + j];
+            all_ps_1[j] = all_ps[16 * i + j + 4];
+            all_ps_2[j] = all_ps[16 * i + j + 8];
+            all_ps_3[j] = all_ps[16 * i + j + 12];
+        } 
+        auto w0 = vxxxxx(all_ps_0, ZERO, hel[0], -1);
+        //Tensor w0 = vxxxxx(all_ps[:,0],ZERO,hel[0],-1)
+    }
     
     return Tensor(); // dummy tensor
 }
 
-Tensor vxxxxx(double p, double vmass, double nhel, double nsv) {
+Tensor complex_tf(Scope& scope, double real, double imag) {
+     /*Complex(
+  const ::tensorflow::Scope & scope,
+  ::tensorflow::Input real,
+  ::tensorflow::Input imag
+)*/
+    return Tensor(); // dummy tensor
+//    return tensorflow::ops::Complex(scope, real, imag);
+}
+
+Tensor vxxxxx(double* p, double vmass, double nhel, double nsv) {
+    //Scope root = Scope::NewRootScope();
+    //Tensor v0 = tensorflow::ops::ExpandDims(root, complex_tf(root, 1, 1), 1);
+    //v0 = tf.expand_dims(complex_tf(p[:, 0] * nsv, p[:, 3] * nsv), 1)
+    //v1 = tf.expand_dims(complex_tf(p[:, 1] * nsv, p[:, 2] * nsv), 1)
+    //pt2 = p[:, 1] ** 2 + p[:, 2] ** 2
+    //pp = tfmath.minimum(p[:, 0], tfmath.sqrt(pt2 + p[:, 3] ** 2))
+    //pt = tfmath.minimum(pp, tfmath.sqrt(pt2))
+    
+    double pt2 = p[1] * p[1] + p[2] * p[2];
     return Tensor(); // dummy tensor
 }
 
