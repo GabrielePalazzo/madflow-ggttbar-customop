ac0d78f7605c2f62e369ad5e23c702e0550431e1
Gabriele <gabriele.palazzo6.github@gmail.com>
2021-07-09 10:34:54 +0200

    Added FFV1_1

--- cpu/matrix.cc
+++ cpu/matrix.cc
@@ -56,7 +56,8 @@ std::vector<complex128> _vx_no_BRST_check_massless_pt_zero(double* p, double nhe
 
 std::vector<complex128> VVV1P0_1(std::vector<complex128> V2, std::vector<complex128> V3, const complex128 COUP, double M1, double W1);
 complex128 FFV1_0(std::vector<complex128> F1, std::vector<complex128> F2, std::vector<complex128> V3, const complex128 COUP);
-std::vector<complex128> FFV1_1(std::vector<complex128> F2, std::vector<complex128> V3, const complex128 COUP_compCOUP, double M1, double W1);
+std::vector<complex128> FFV1_1(std::vector<complex128> F2, std::vector<complex128> V3, const complex128 COUP, double M1, double W1);
+std::vector<complex128> FFV1_2(std::vector<complex128> F1, std::vector<complex128> V3, const complex128 COUP, double M1, double W1);
 
 double sign(double x, double y);
 double signvec(double x, double y);
@@ -134,12 +135,12 @@ Tensor matrix(const double* all_ps, const double* hel, const double mdl_MT, cons
         
         auto amp0 = FFV1_0(w3, w2, w4, GC_11);
         w4 = FFV1_1(w2, w0, GC_11, mdl_MT, mdl_WT);
+        
+        // Amplitude(s) for diagram number 2
+        
+        auto amp1 = FFV1_0(w3,w4,w1,GC_11);
+        w4= FFV1_2(w3, w0, GC_11, mdl_MT, mdl_WT);
         /*
-        amp0= FFV1_0(w3,w2,w4,GC_11)
-        w4= FFV1_1(w2,w0,GC_11,mdl_MT,mdl_WT)
-        # Amplitude(s) for diagram number 2
-        amp1= FFV1_0(w3,w4,w1,GC_11)
-        w4= FFV1_2(w3,w0,GC_11,mdl_MT,mdl_WT)
         # Amplitude(s) for diagram number 3
         amp2= FFV1_0(w4,w2,w1,GC_11)
 
@@ -628,6 +629,42 @@ std::vector<complex128> FFV1_1(std::vector<complex128> F2, std::vector<complex12
     
     return F1;
 }
+
+std::vector<complex128> FFV1_2(std::vector<complex128> F1, std::vector<complex128> V3, const complex128 COUP_comp, double M2_double, double W2_double) {
+    complex128 cI(0, 1);
+    complex128 M2 = M2_double;
+    complex128 W2 = W2_double;
+    complex128 COUP = COUP_comp;
+    
+    std::vector<complex128> F2(6, complex128(0,0));
+    F2[0] = F1[0] + V3[0];
+    F2[1] = F1[1] + V3[1];
+    
+    std::vector<complex128> P2(4, complex128(0,0));
+    P2[0] = complex128(-F2[0].real(), 0.0);
+    P2[1] = complex128(-F2[1].real(), 0.0);
+    P2[2] = complex128(-F2[1].imag(), 0.0);
+    P2[3] = complex128(-F2[0].imag(), 0.0);
+    
+    complex128 denom = COUP/(P2[0]*P2[0] - P2[1]*P2[1] - P2[2]*P2[2] - P2[3]*P2[3] - M2 * (M2 -cI* W2));
+    
+    F2[2]= denom*cI*(F1[2]*(P2[0]*(V3[2]+V3[5])+(P2[1]*(-1./1.)*(V3[3]+cI*(V3[4]))+(P2[2]*(cI*(V3[3])-V3[4])-P2[3]*(V3[2]+V3[5]))))+(F1[3]*(P2[0]*(V3[3]-cI*(V3[4]))+(P2[1]*(-V3[2]+V3[5])+(P2[2]*(cI*(V3[2])-cI*(V3[5]))+P2[3]*(-V3[3]+cI*(V3[4])))))+M2*(F1[4]*(V3[2]-V3[5])+F1[5]*(-V3[3]+cI*(V3[4])))));
+    F2[3]= denom*(-cI)*(F1[2]*(P2[0]*(-1./1.)*(V3[3]+cI*(V3[4]))+(P2[1]*(V3[2]+V3[5])+(P2[2]*(cI*(V3[2]+V3[5]))-P2[3]*(V3[3]+cI*(V3[4])))))+(F1[3]*(P2[0]*(-V3[2]+V3[5])+(P2[1]*(V3[3]-cI*(V3[4]))+(P2[2]*(cI*(V3[3])+V3[4])+P2[3]*(-V3[2]+V3[5]))))+M2*(F1[4]*(V3[3]+cI*(V3[4]))-F1[5]*(V3[2]+V3[5]))));
+    F2[4]= denom*(-cI)*(F1[4]*(P2[0]*(-V3[2]+V3[5])+(P2[1]*(V3[3]+cI*(V3[4]))+(P2[2]*(-cI*(V3[3])+V3[4])+P2[3]*(-V3[2]+V3[5]))))+(F1[5]*(P2[0]*(V3[3]-cI*(V3[4]))+(P2[1]*(-1./1.)*(V3[2]+V3[5])+(P2[2]*(cI*(V3[2]+V3[5]))+P2[3]*(V3[3]-cI*(V3[4])))))+M2*(F1[2]*(-1./1.)*(V3[2]+V3[5])+F1[3]*(-V3[3]+cI*(V3[4])))));
+    F2[5]= denom*cI*(F1[4]*(P2[0]*(-1./1.)*(V3[3]+cI*(V3[4]))+(P2[1]*(V3[2]-V3[5])+(P2[2]*(cI*(V3[2])-cI*(V3[5]))+P2[3]*(V3[3]+cI*(V3[4])))))+(F1[5]*(P2[0]*(V3[2]+V3[5])+(P2[1]*(-V3[3]+cI*(V3[4]))+(P2[2]*(-1./1.)*(cI*(V3[3])+V3[4])-P2[3]*(V3[2]+V3[5]))))+M2*(F1[2]*(V3[3]+cI*(V3[4]))+F1[3]*(V3[2]-V3[5]))));
+    
+    return F2;
+}/*
+    F2 = [complex_tf(0,0)] * 6
+    F2[0] = F1[0]+V3[0]
+    F2[1] = F1[1]+V3[1]
+    P2 = complex_tf(tf.stack([-tf.math.real(F2[0]), -tf.math.real(F2[1]), -tf.math.imag(F2[1]), -tf.math.imag(F2[0])], axis=0), 0.)
+    denom = COUP/(P2[0]**2-P2[1]**2-P2[2]**2-P2[3]**2 - M2 * (M2 -cI* W2))
+    F2[2]= denom*cI*(F1[2]*(P2[0]*(V3[2]+V3[5])+(P2[1]*(-1./1.)*(V3[3]+cI*(V3[4]))+(P2[2]*(cI*(V3[3])-V3[4])-P2[3]*(V3[2]+V3[5]))))+(F1[3]*(P2[0]*(V3[3]-cI*(V3[4]))+(P2[1]*(-V3[2]+V3[5])+(P2[2]*(cI*(V3[2])-cI*(V3[5]))+P2[3]*(-V3[3]+cI*(V3[4])))))+M2*(F1[4]*(V3[2]-V3[5])+F1[5]*(-V3[3]+cI*(V3[4])))))
+    F2[3]= denom*(-cI)*(F1[2]*(P2[0]*(-1./1.)*(V3[3]+cI*(V3[4]))+(P2[1]*(V3[2]+V3[5])+(P2[2]*(cI*(V3[2]+V3[5]))-P2[3]*(V3[3]+cI*(V3[4])))))+(F1[3]*(P2[0]*(-V3[2]+V3[5])+(P2[1]*(V3[3]-cI*(V3[4]))+(P2[2]*(cI*(V3[3])+V3[4])+P2[3]*(-V3[2]+V3[5]))))+M2*(F1[4]*(V3[3]+cI*(V3[4]))-F1[5]*(V3[2]+V3[5]))))
+    F2[4]= denom*(-cI)*(F1[4]*(P2[0]*(-V3[2]+V3[5])+(P2[1]*(V3[3]+cI*(V3[4]))+(P2[2]*(-cI*(V3[3])+V3[4])+P2[3]*(-V3[2]+V3[5]))))+(F1[5]*(P2[0]*(V3[3]-cI*(V3[4]))+(P2[1]*(-1./1.)*(V3[2]+V3[5])+(P2[2]*(cI*(V3[2]+V3[5]))+P2[3]*(V3[3]-cI*(V3[4])))))+M2*(F1[2]*(-1./1.)*(V3[2]+V3[5])+F1[3]*(-V3[3]+cI*(V3[4])))))
+    F2[5]= denom*cI*(F1[4]*(P2[0]*(-1./1.)*(V3[3]+cI*(V3[4]))+(P2[1]*(V3[2]-V3[5])+(P2[2]*(cI*(V3[2])-cI*(V3[5]))+P2[3]*(V3[3]+cI*(V3[4])))))+(F1[5]*(P2[0]*(V3[2]+V3[5])+(P2[1]*(-V3[3]+cI*(V3[4]))+(P2[2]*(-1./1.)*(cI*(V3[3])+V3[4])-P2[3]*(V3[2]+V3[5]))))+M2*(F1[2]*(V3[3]+cI*(V3[4]))+F1[3]*(V3[2]-V3[5]))))
+    return tf.stack(F2, axis=0)*/
 
 REGISTER_KERNEL_BUILDER(Name("Matrix").Device(DEVICE_CPU), MatrixOp);
 
